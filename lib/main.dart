1a3fb4a35e5d22977c158b7a2b460880e39eb828
Valhalla <valhallamaia@gmail.com>
2024-08-15 22:48:39 -0300

    Atualizacao Aula 15-08-2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 // Metodo Main
-void main(){
+void main() {
   runApp(const Sextou());
 }
 
@@ -11,9 +14,41 @@ class Sextou extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Balada Segura',
-      home: Text("HotDogger")
-      );
+    return MaterialApp(
+        title: 'Balada Segura',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              "HotDogger",
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+          ),
+          body: Container(
+            margin: EdgeInsets.all(20),
+            padding: EdgeInsets.all(10),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Panchito - 2.00"),
+                Text("Pancho Completo - 15.00"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text("Pancho da Linha - 18.00",
+                    style: TextStyle(fontWeight: FontWeight.bold),),
+                  ],
+                ),
+                Text("PanchO com panceta - 9.00"),
+                Text("Pancho Com bacon - 25.00")
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(onPressed: null, backgroundColor: Colors.red),
+        ));
   }
 }
